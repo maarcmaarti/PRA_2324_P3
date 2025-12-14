7438cce5d5d353a2b27559f4cefb943cd378fdc7
maarcmaarti <mmaroch1@upv.edu.es>
2025-12-14 11:13:42 +0100

    añadidos operadores que faltaban

--- TableEntry.h
+++ TableEntry.h
@@ -10,44 +10,39 @@ public:
     std::string key;
     V value;
 
-    // Constructor clave-valor
     TableEntry(const std::string& key, const V& value) {
         this->key = key;
         this->value = value;
     }
 
-    // Constructor solo clave (para búsquedas y borrados)
     TableEntry(const std::string& key) {
         this->key = key;
-        // value queda sin inicializar intencionalmente,
-        // porque no se usa en las búsquedas
     }
 
-    // Constructor por defecto
     TableEntry() : key(""), value() {}
 
-    // Comparaciones por clave
-    friend bool operator<(const TableEntry<V>& t1, const TableEntry<V>& t2) {
-        return t1.key < t2.key;
+    friend bool operator==(const TableEntry<V>& te1, const TableEntry<V>& te2) {
+        return te1.key == te2.key;
     }
 
-    friend bool operator>(const TableEntry<V>& t1, const TableEntry<V>& t2) {
-        return t1.key > t2.key;
+    friend bool operator!=(const TableEntry<V>& te1, const TableEntry<V>& te2) {
+        return te1.key != te2.key;
     }
 
-    friend bool operator==(const TableEntry<V>& t1, const TableEntry<V>& t2) {
-        return t1.key == t2.key;
-    }
-
-    friend bool operator!=(const TableEntry<V>& t1, const TableEntry<V>& t2) {
-        return t1.key != t2.key;
-    }
-
-    // Impresión
     friend std::ostream& operator<<(std::ostream& out, const TableEntry<V>& te) {
         out << te.key << "->" << te.value;
         return out;
     }
+
+    friend bool operator<(const TableEntry<V>& te1, const TableEntry<V>& te2) {
+        return te1.key < te2.key ? true:false;
+    }
+
+    friend bool operator>(const TableEntry<V>& te1, const TableEntry<V>& te2) {
+        return te1.key > te2.key ? true:false;
+    }
+
+
 };
 
 #endif
